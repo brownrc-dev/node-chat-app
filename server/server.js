c70487858df5e9f640d0e43c859d7d70d70fba4d
Robert Brown <brownrc@live.com>
2017-12-02 18:23:20 -0600

    Updated info

--- server/server.js
+++ server/server.js
@@ -5,14 +5,13 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
-var app = express();
-
-app.use(express.static(publicPath));
-
 var port = process.env.PORT || 3000;
 
+var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
